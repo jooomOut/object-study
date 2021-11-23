ab33be41071caa894cf4815b47083fa84bc41168
jooomOut <rimeilo324@naver.com>
2021-11-23 22:17:37 +0900

    Refactor: Audience의 bag 접근 로직을 Audience 내부로 캡슐화

--- src/jooom/chapter01/Audience.java
+++ src/jooom/chapter01/Audience.java
@@ -10,4 +10,15 @@ public class Audience {
     public Bag getBag(){
         return bag;
     }
+
+    public Long Buy(Ticket ticket){
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L; // 현금을 주고 사지 않았으니 0원을 반환합니다.
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
 }
