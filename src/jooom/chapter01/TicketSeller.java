cc540d017064cc97b4a9621624de31376549dc80
jooomOut <rimeilo324@naver.com>
2021-11-23 21:28:24 +0900

    Feat: Audience, ticker 관련 클래스 구현
    관람객, 매표소와 티켓 판매원 클래스

---
+++ src/jooom/chapter01/TicketSeller.java
@@ -0,0 +1,13 @@
+package jooom.chapter01;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice){
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice(){
+        return ticketOffice;
+    }
+}
