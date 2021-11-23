a39857e31ab0d8e73f6061177896bf05ec01dc49
jooomOut <rimeilo324@naver.com>
2021-11-23 21:17:01 +0900

    Feat: 소지품을 넣을 Bag 구현
    현금, 티켓, 초대장을 가지고 있음

---
+++ src/jooom/chapter01/Bag.java
@@ -0,0 +1,26 @@
+package jooom.chapter01;
+
+public class Bag {
+    private Long amount; // 현금
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public boolean hasInvitation(){
+        return invitation != null;
+    }
+
+    public boolean hasTicket(){
+        return ticket != null;
+    }
+
+    public void setTicket(Ticket ticket){
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount){
+        this.amount -= amount;
+    }
+    public void plusAmount(Long amount){
+        this.amount += amount;
+    }
+}
