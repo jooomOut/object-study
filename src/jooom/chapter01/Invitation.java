50bc386d2724b8f6d840933701141db94bbc8ec0
jooomOut <rimeilo324@naver.com>
2021-11-23 21:13:26 +0900

    초대장과 티켓 구현

---
+++ src/jooom/chapter01/Invitation.java
@@ -0,0 +1,7 @@
+package jooom.chapter01;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ src/jooom/chapter01/Ticket.java
@@ -0,0 +1,9 @@
+package jooom.chapter01;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee(){
+        return fee;
+    }
+}
