69f339448e0eec1021ce903973576c02d3d62ffa
jooomOut <rimeilo324@naver.com>
2021-12-22 19:21:05 +0900

    Bag 가져오지 않고 Audience에게 위임

--- src/jooom/chapter06/TicketSeller.java
+++ src/jooom/chapter06/TicketSeller.java
@@ -11,15 +11,7 @@ public class TicketSeller {
         return this.ticketOffice;
     }
     public void setTicket(Audience audience) {
-        if (audience.getBag().hasInvitation()){
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketOffice.plusAmount(audience.setTicket(ticketOffice.getTicket()));
     }
 
     public void sellTo(Audience audience){
