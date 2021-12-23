e8bf2d8bc79bc100a861e5336a442b9108b7118f
jooomOut <rimeilo324@naver.com>
2021-12-23 21:11:01 +0900

    소득세율통한 실 급여 계산

--- src/jooom/chapter07/TopDown.java
+++ src/jooom/chapter07/TopDown.java
@@ -3,15 +3,20 @@ package jooom.chapter07;
 import java.util.Scanner;
 
 public class TopDown {
+    private static double salary = 300;
     /*직원의 급여를 계산한다 */
     public static void main(String[] args) {
         double percent = inputPercent();
-        // TODO: 급여 계산
+        double calculatedSalary = calculateSalary(percent);
         // TODO: 출력
     }
     private static double inputPercent(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("소득세율 입력 : ");
         return scanner.nextDouble();
+    }
+
+    private static double calculateSalary(double percent){
+        return salary * (1 - percent);
     }
 }
