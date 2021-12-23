95a9b45cb77c907a472590667ad03cb090713f26
jooomOut <rimeilo324@naver.com>
2021-12-23 21:05:07 +0900

    chap07 하향식 접근 문제

---
+++ src/jooom/chapter07/TopDown.java
@@ -0,0 +1,11 @@
+package jooom.chapter07;
+
+public class TopDown {
+    /*직원의 급여를 계산한다 */
+    public static void main(String[] args) {
+        // TODO: 소득세율 입력
+        // TODO: 급여 계산
+        // TODO: 출력력
+    }
+
+}
