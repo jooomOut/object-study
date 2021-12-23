ff5b41f645bea0ff91345a31dcbe207e85fbf218
jooomOut <rimeilo324@naver.com>
2021-12-23 21:08:09 +0900

    소득 세율 입력

--- src/jooom/chapter07/TopDown.java
+++ src/jooom/chapter07/TopDown.java
@@ -1,11 +1,17 @@
 package jooom.chapter07;
 
+import java.util.Scanner;
+
 public class TopDown {
     /*직원의 급여를 계산한다 */
     public static void main(String[] args) {
-        // TODO: 소득세율 입력
+        double percent = inputPercent();
         // TODO: 급여 계산
-        // TODO: 출력력
+        // TODO: 출력
     }
-
+    private static double inputPercent(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("소득세율 입력 : ");
+        return scanner.nextDouble();
+    }
 }
