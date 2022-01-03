a6ac7bf658e758782344d1cbb414eb20c89305c3
jooomOut <rimeilo324@naver.com>
2022-01-03 19:15:35 +0900

    InstrumentedHashSet 포워딩 방식으로 구현
    Set 을 구현하고 HashSet을 인스턴스 변수로 가진다.

--- src/jooom/chapter11/InstrumentedHashSet.java
+++ src/jooom/chapter11/InstrumentedHashSet.java
@@ -1,7 +1,9 @@
 package jooom.chapter11;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.Spliterator;
 
 
 /**
@@ -9,7 +11,7 @@ import java.util.Set;
  *
  * 상속 관계를 제거하고 합성을 사용해 count 오류를 제거
  * */
-public class InstrumentedHashSet<E> {
+public class InstrumentedHashSet<E> implements Set<E> {
     private int addCount = 0;
     private Set<E> set;
 
@@ -17,11 +19,13 @@ public class InstrumentedHashSet<E> {
         this.set = set;
     }
 
+    @Override
     public boolean add(E e){
         addCount++;
         return set.add(e);
     }
 
+    @Override
     public boolean addAll(Collection<? extends E> c){
         addCount += c.size();
         return set.addAll(c);
@@ -30,4 +34,19 @@ public class InstrumentedHashSet<E> {
     public int getAddCount(){
         return addCount;
     }
+
+    @Override public int size() {return set.size();}
+    @Override public boolean isEmpty() {return set.isEmpty();}
+    @Override public boolean contains(Object o) {return set.contains(o);}
+    @Override public Iterator<E> iterator() {return set.iterator();}
+    @Override public Object[] toArray() {return set.toArray();}
+    @Override public <T> T[] toArray(T[] a) {return set.toArray(a);}
+    @Override public boolean remove(Object o) {return set.remove(o);}
+    @Override public boolean containsAll(Collection<?> c) {return set.containsAll(c);}
+    @Override public boolean retainAll(Collection<?> c) {return set.retainAll(c);}
+    @Override public boolean removeAll(Collection<?> c) {return set.removeAll(c);}
+    @Override public void clear() {set.clear();}
+    @Override public boolean equals(Object o) {return set.equals(o);}
+    @Override public  int hashCode() {return set.hashCode();}
+    @Override public Spliterator<E> spliterator() {return set.spliterator();}
 }
