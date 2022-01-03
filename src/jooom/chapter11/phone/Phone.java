466b8d18b93e38a765ec49497ec88f753813f5f7
jooomOut <rimeilo324@naver.com>
2022-01-03 19:18:38 +0900

    [조합 예제 초기화]

---
+++ src/jooom/chapter11/phone/Phone.java
@@ -0,0 +1,20 @@
+package jooom.chapter11.phone;
+
+import jooom.chapter10.Money;
+
+import java.time.Duration;
+
+public class Phone extends AbstractPhone {
+    private Money amount;
+    private Duration seconds;
+
+    public Phone(Money amount, Duration seconds) {
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    @Override
+    protected Money calculateCallFee(Call call) {
+        return amount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+    }
+}
