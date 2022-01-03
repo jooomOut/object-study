64643a318874fb440288c9b404a3dba3bdd103a2
jooomOut <rimeilo324@naver.com>
2022-01-03 17:55:45 +0900

    불필요한 인터페이스 상속의 문제점 주석 추가

--- src/jooom/chapter11/Properties.java
+++ src/jooom/chapter11/Properties.java
@@ -2,6 +2,12 @@ package jooom.chapter11;
 
 import java.util.Hashtable;
 
+/**
+ * 불필요한 인터페이스의 상속으로 생기는 문제
+ *
+ * Properties는 String 타입만 허용하는데 HashSet의 인터페이스는
+ * 그렇지 않아 문제가 생길 수 있음. 이를 상속에서 합성으로 변경해 해결한다.
+ * */
 public class Properties {
     private Hashtable<String, String> properties = new Hashtable<>();
 
--- src/jooom/chapter11/Stack.java
+++ src/jooom/chapter11/Stack.java
@@ -3,6 +3,12 @@ package jooom.chapter11;
 import java.util.EmptyStackException;
 import java.util.Vector;
 
+/**
+ * 불필요한 인터페이스의 상속으로 생기는 문제
+ *
+ * Stack의 FIFO 구조가 Vector의 중간 삽입 메서드로 깨질 우려가 있어
+ * 합성으로 변경한다.
+ * */
 public class Stack<E> {
     private Vector<E> elements = new Vector<>();
 
