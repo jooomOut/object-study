ffc301b0d0ae6156c91bbfaa3b404a8c4f193b70
jooomOut <rimeilo324@naver.com>
2022-01-05 17:45:19 +0900

    등급 계산 기능 추가
    super 참조사용해 구현함, super 참조 지양해야 하지 않을까 의문

--- src/jooom/chapter12/GradeLecture.java
+++ src/jooom/chapter12/GradeLecture.java
@@ -2,11 +2,34 @@ package jooom.chapter12;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.joining;
+
 public class GradeLecture extends Lecture {
     private List<Grade> grades;
 
     public GradeLecture(int pass, String title, List<Integer> scores, List<Grade> grades) {
         super(pass, title, scores);
         this.grades = grades;
+    }
+
+    @Override
+    public String evaluate(){
+        return super.evaluate() + ", " + gradesStatistics();
+    }
+
+    private String gradesStatistics(){
+        return grades.stream()
+                .map(this::format)
+                .collect(joining(" "));
+    }
+
+    private String format(Grade grade){
+        return String.format("%s:%d", grade.getName(), gradeCount(grade));
+    }
+
+    private long gradeCount(Grade grade){
+        return getScores().stream()
+                .filter(grade::include)
+                .count();
     }
 }
